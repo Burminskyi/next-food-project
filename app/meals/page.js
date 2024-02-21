8b930b0bcf1343f9765d2e43f565e5be529ae93f
Burminskyi <burminskyi91@gmail.com>
2024-02-21 12:26:56 +0200

    added metadata for meals pages

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,15 @@
+import { Suspense } from "react";
 import Link from "next/link";
 
 import styles from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
-import { Suspense } from "react";
+
+export const metadata = {
+  title: 'All meals',
+  description: 'Browse the delicious meals shared by our vibrant community.'
+}
+
 
 async function Meals() {
   const meals = await getMeals();
